2f22fb59b7547e8c6bd38f647887532b79b2b024
Aryan Sengar <17aryangs2003@gmail.com>
2024-07-03 04:59:50 +0000

    eror push

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Header from './components/Header';
-import HeaderCarousel from './components/Homepage/HeaderCarousel';
+import Header from './components/Homepage/Header';
+import HeaderCarousel from './Pages/HeaderCarousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Marquee from './components/Homepage/Marquee';
-import PlacementSection from './components/Homepage/PlacementSection';
-import Keypoints from './components/Homepage/Keypoints';
-import PopCourses from './components/Homepage/PopCourses';
+import Marquee from './Pages/Marquee';
+import PlacementSection from './Pages/PlacementSection';
+import Keypoints from './Pages/Keypoints';
+import PopCourses from './Pages/PopCourses';
 
 
 function App() {
