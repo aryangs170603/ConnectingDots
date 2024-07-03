361f01540e2812e3ecaf01a25876f025609aa110
Aryan Sengar <17aryangs2003@gmail.com>
2024-07-03 05:25:28 +0000

    modified:   src/App.css 	new file:   src/components/Homepage/Header.css 	modified:   src/components/Homepage/Header.jsx

--- src/components/Homepage/Header.jsx
+++ src/components/Homepage/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from '/src/Logos/cderplogo white bg.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import '../App.css';
+import './Header.css';
 
 const Header = () => (
   <header> 
